b5fab8a5a73ce1065bdc3704c44eb8d529602a91
dsabirbaev <davran9559@gmail.com>
2024-02-07 02:13:43 +0500

    add profile func

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,16 +1,38 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import { Input } from "@/components/ui/input"
+import { ACCOUNT } from "@/lib/appwrite"
 
 
 const Profile = () => {
+  const[name, setName] = useState<String>('');
+  const[email, setEmail] = useState<String>('');
 
-  const { email, name} = JSON.parse(localStorage.getItem("user"));
+  const getData = async() => {
+
+    try{
+      const response = await ACCOUNT.get('current');
+      setName(response.name)
+      setEmail(response.email)
+    }catch(error){
+      console.log(error)
+    }
+  }
+  
+  useEffect(() => {
+    getData();
+  }, [])
 
   return (
     <section>
-      <div className="flex items-center justify-center w-full h-screen">
-          
+      <div className="flex items-center justify-center w-full h-screen flex-col">
+       <h1 className="mb-5 text-4xl font-bold">
+        { 
+         
+         `${name.charAt(0).toUpperCase()}${name.slice(1)}'s profile `
+        }
+       </h1>
        <div className="border rounded-md p-5 shadow w-[400px]">
         <label htmlFor="name">
           <span className="text-[15px] text-gray-400 mb-1">Name</span>
